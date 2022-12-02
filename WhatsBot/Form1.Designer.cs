a0d506f7f2211cc5bfcf06ad2ce514b01649a65c
Gabriel Pinheiro Ximenes <gximenes@ceapema.matriz>
2022-12-02 12:33:23 -0300

    First Commit

---
+++ WhatsBot/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZapBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxContato = new System.Windows.Forms.TextBox();
+            this.textBoxMensagem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome Contato:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enviar!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxContato
+            // 
+            this.textBoxContato.Location = new System.Drawing.Point(110, 29);
+            this.textBoxContato.Name = "textBoxContato";
+            this.textBoxContato.Size = new System.Drawing.Size(100, 20);
+            this.textBoxContato.TabIndex = 2;
+            // 
+            // textBoxMensagem
+            // 
+            this.textBoxMensagem.Location = new System.Drawing.Point(110, 69);
+            this.textBoxMensagem.Name = "textBoxMensagem";
+            this.textBoxMensagem.Size = new System.Drawing.Size(247, 20);
+            this.textBoxMensagem.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mensagem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 127);
+            this.Controls.Add(this.textBoxMensagem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxContato);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxContato;
+        private System.Windows.Forms.TextBox textBoxMensagem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WhatsBot/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+
+namespace ZapBot
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver;
+
+        public Form1()
+
+        {
+            InitializeComponent();
+            driver = new EdgeDriver("Path to Chrome Driver");
+            openPage();
+
+        }
+
+        private void openPage()
+        {
+            driver.Url = "https://web.whatsapp.com/";
+            Thread.Sleep(150);
+        }
+
+        private void openChat(string contactName)
+        {
+            while (true)
+            {
+                try
+                {
+                    var newChatBox = driver.FindElement(By.CssSelector($"span[data-testid='chat']"));
+                    newChatBox.Click();
+                    break;
+                }
+                catch (Exception ex){ Thread.Sleep(50); }
+            }
+            while (true)
+            {
+                try
+                {
+                    var searchBar = driver.FindElement(By.CssSelector($"div[data-testid='chat-list-search']"));
+                    searchBar.SendKeys(contactName);
+                    searchBar.SendKeys(OpenQA.Selenium.Keys.Enter);
+                    break;
+                }
+                catch (Exception ex) { Thread.Sleep(50); }
+             }
+
+        }
+
+        private void sendMessage(string message)
+        {   
+            while (true)
+            {
+                try
+                    {
+
+                        var textBar = driver.FindElement(By.ClassName(("p3_M1")));
+                        textBar.SendKeys(message);
+                        var sendButton = driver.FindElement(By.CssSelector("span[data-testid='send']"));
+                        sendButton.Click();
+                        break;
+                    }
+            catch (Exception ex) { Thread.Sleep(50); }
+
+            }
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string message = textBoxMensagem.Text;
+            string contact = textBoxContato.Text;
+
+            
+            openChat(contact);
+            sendMessage(message);
+            
+        }
+    }
+}
